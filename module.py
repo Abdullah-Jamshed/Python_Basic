218a8f83366e390bc61a5a9e450ee86f5ece280f
Abdullah Jamshed <abdullahjamshedkarachi@gmail.com>
2019-10-23 06:51:17 +0500

    python basic

---
+++ module.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(a,b):
+    return a+b
+def subract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
